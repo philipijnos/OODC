33cb4629d066e143f5c6ca35ca6a393d810bd22b
philipijnos <philie753@gmail.com>
2016-02-22 13:52:43 +0100

    Leeggemaakt om eindelijk echte opdrachten op te kunnen zetten

--- V1OODC/src/test/Main.java
+++
@@ -1,6 +0,0 @@
-package test;
-public class Main {
-	public static void main(String[] arg) {
-		System.out.println("Hello world!");
-	}
-}
--- V1OODC/src/test2/Main.java
+++
@@ -1,6 +0,0 @@
-package test2;
-public class Main {
-	public static void main(String[] arg) {
-		System.out.println("Bye World!");
-	}
-}
