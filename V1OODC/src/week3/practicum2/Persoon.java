c3607e3647d8cba9f771615afc9a44b9463c3120
philipijnos <philie753@gmail.com>
2016-02-22 22:44:06 +0100

    Practicum 2 week 3 zo goed als af

---
+++ V1OODC/src/week3/practicum2/Persoon.java
@@ -0,0 +1,64 @@
+package week3.practicum2;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Persoon{
+	private String naam;
+	private double budget;
+	private ArrayList<Game> mijnGames;
+
+	public Persoon(String nm, double bud){
+		naam = nm;
+		budget = bud;
+		mijnGames = new ArrayList<Game>();
+	}
+	public boolean koop(Game inputGame){
+		boolean result = true;
+		if(budget >= inputGame.huidigeWaarde()){
+			for(Game g : mijnGames){
+				if(g.equals(inputGame)){
+					result = false;
+					break;
+				}
+			}
+			if(result){
+				mijnGames.add(inputGame);
+				budget -= inputGame.huidigeWaarde();
+			}
+		}
+		return result;
+	}
+	public boolean verkoop(Game inputGame, Persoon inputPersoon){
+		boolean result = true;
+		for(Game g : this.mijnGames){
+			if(inputGame.equals(g) && inputPersoon.budget >= inputGame.huidigeWaarde()){
+				for(Game h : inputPersoon.mijnGames){
+					if(h.equals(inputGame)){
+						result = false;
+						break;
+					}
+				}
+				break;
+			}
+			else{
+				result = false;
+			}
+		}
+		if(result){
+			inputPersoon.mijnGames.add(inputGame);
+			inputPersoon.budget -= inputGame.huidigeWaarde();
+			this.mijnGames.remove(inputGame);
+			this.budget += inputGame.huidigeWaarde();
+		}
+		return result;
+	}
+	public String toString(){
+		DecimalFormat df = new DecimalFormat("#.##");
+		String result = naam+" heeft een budget van €"+df.format(budget)+" en bezit de volgende games:";
+		for(Game g : mijnGames){
+			result += "\n"+g;
+		}
+		return result;
+	}
+
+}
