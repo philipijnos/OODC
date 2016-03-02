b0b895304faa23155dee98c325683dce493a5c28
philipijnos <philie753@gmail.com>
2016-03-02 12:21:40 +0100

    Thuis verder werken, opdrachten in les af.

---
+++ V1OODC/src/week4/les7/opdracht1/Gebouw.java
@@ -0,0 +1,12 @@
+package week4.les7.opdracht1;
+
+public class Gebouw{
+	private int lengte;
+	private int breedte;
+	private int aantalVerdiepingen;
+	public int laatsteRenovatie;
+	
+	public Gebouw(){
+		
+	}
+}
